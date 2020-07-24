31050bf3b4548503f3aeda9f67ec0b113101a1d6
ITsupport <it_support@ebet.com>
2020-07-24 15:43:01 +0800

    刪除美元

--- js/header_css.js
+++ js/header_css.js
@@ -7,7 +7,8 @@ h_css.innerHTML = `
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.15.5/dist/bootstrap-table.min.css">
+<link href="https://fonts.googleapis.com/css?family=Noto+Sans+SC&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="css/api.css">
 <link rel="icon" type="image/x-icon" href="/img/favicon.ico"/>
 <link rel="stylesheet" type="text/css" href="./swagger-ui.css">
-<link href="https://fonts.googleapis.com/css?family=Noto+Sans+SC&display=swap" rel="stylesheet">`;
+`;
