93d8b8ddd6c3b655705d28b35224d8c9671537e2
IT_support <it_support@ebet.tw>
2021-06-13 18:11:49 +0800

    真人21更新

--- js/header.js
+++ js/header.js
@@ -58,6 +58,7 @@ if (window.localStorage.getItem('lang') == "cn") {
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="blackjack.html">21点</a>
+                        <a class="dropdown-item" href="blackjacklive.html">21点</a>
                         <!--
                         <a class="dropdown-item" href="fortune_wheel.html">财富大转盘</a>
                         -->
@@ -147,6 +148,7 @@ if (window.localStorage.getItem('lang') == "cn") {
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="blackjack.html">BlackJack</a>
+                        <a class="dropdown-item" href="blackjacklive.html">BlackJack Live</a>
                         <!--
                         <a class="dropdown-item" href="fortune_wheel.html">Fortune Wheel</a>
                         -->
