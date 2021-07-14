8d1e0cd169e748825ff1f9f81708b90ce8790d03
IT_support <it_support@ebet.tw>
2021-07-14 16:38:19 +0800

    gameType增加更新

--- js/gameType.js
+++ js/gameType.js
@@ -1,51 +1,19 @@
 const gameType= [
-    {
-        code: "-1",
-        GameType: "Games Lobby"
-    },
-    {
-        code: "0",
-        GameType: "multi-Baccarat"
-    },
-    {
-        code: "1",
-        GameType: "Baccarat"
-    }, {
-        code: "2",
-        GameType: "Dragon Tiger"
-    }, {
-        code: "3",
-        GameType: "Roulette"
-    }, {
-        code: "4",
-        GameType: "Sic Bo (Dice)"
-    }, {
-        code: "5",
-        GameType: "Slot"
-    },
-    //  {
-    //     code: "6",
-    //     GameType: "Trial slot"
-    // },
-     {
-        code: "7",
-        GameType: "Blockchain baccarat"
-    }, {
-        code: "8",
-        GameType: "BullBull"
-    }, {
-        code: "22",
-        GameType: "Lottery"
-    }, {
-        code: "23",
-        GameType: "Fortune Wheel"
-    }, {
-        code: "24",
-        GameType: "Black Jack"
-    }, {
-        code: "25",
-        GameType: ""
-    }]
+    {code: "-1",GameType: "Games Lobby"},
+    {code: "0",GameType: "multi-Baccarat"},
+    {code: "1",GameType: "Baccarat"}, 
+    {code: "2",GameType: "Dragon Tiger"},
+    {code: "3",GameType: "Roulette"}, 
+    {code: "4",GameType: "Sic Bo (Dice)"}, 
+    {code: "5",GameType: "Slot"},
+    // {code: "6",GameType: "Trial slot"},
+    // {code: "7",GameType: "Blockchain baccarat"}, 
+    {code: "8",GameType: "BullBull"}, 
+    // {code: "22",GameType: "Lottery"}, 
+    // {code: "23",GameType: "Fortune Wheel"}, 
+    {code: "24",GameType: "Black Jack"}, 
+    {code: "25",GameType: "Live Black Jack"}
+]
 
 const gameType_cn = [
     { "code": "-1", "GameType": "大厅" },
@@ -56,9 +24,10 @@ const gameType_cn = [
     { "code": "0", "GameType": "多台(仅提供登入用)" },
     { "code": "5", "GameType": "老虎机" },
     // { "code": "6", "GameType": "试玩老虎机" },
-    { "code": "7", "GameType": "区块链百家乐" },
+    // { "code": "7", "GameType": "区块链百家乐" },
     { "code": "8", "GameType": "牛牛" },
-    { "code": "22", "GameType": "彩票" },
-    { "code": "23", "GameType": "财富大转盘" },
-    { "code": "24", "GameType": "21点(电子)" }
+    // { "code": "22", "GameType": "彩票" },
+    // { "code": "23", "GameType": "财富大转盘" },
+    { "code": "24", "GameType": "21点(电子)" },
+    { "code": "24", "GameType": "真人21点" }
 ]
