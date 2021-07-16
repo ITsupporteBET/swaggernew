d4d9d77e4287004bba8e4ed9474360166dbf2527
IT_support <it_support@ebet.tw>
2021-07-16 16:42:06 +0800

    增加更新0716

--- js/header.js
+++ js/header.js
@@ -57,13 +57,13 @@ if (window.localStorage.getItem('lang') == "cn") {
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">新增游戏
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="blackjack.html">电子21点</a>
+                        <a href="javascript:window.location.reload()" class="dropdown-item" href="blackjack.html">电子21点</a>
                         
                         <!--
-                        <a class="dropdown-item" href="liveblackjack.html">真人21点</a>
+                        <a class="dropdown-item" href="liveblackjack.html">真人21点123</a>
                         -->
                         
-                        <a class="dropdown-item" href="bullbull.html">牛牛</a>
+                        <a href="javascript:window.location.reload()" class="dropdown-item" href="bullbull.html">牛牛</a>
                         
                         <!--
                         <a class="dropdown-item" href="fortune_wheel.html">财富大转盘</a>
@@ -156,7 +156,7 @@ if (window.localStorage.getItem('lang') == "cn") {
                         <a class="dropdown-item" href="blackjack.html">BlackJack</a>
 
                         <!--
-                        <a class="dropdown-item" href="liveblackjack.html">BlackJack Live</a>
+                        <a class="dropdown-item" href="liveblackjack.html">Live BlackJack </a>
                         -->
 
                         <a class="dropdown-item" href="bullbull.html">BullBull</a>
