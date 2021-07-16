cd629873f241af45ccee3d57d5464214f9c5924d
IT_support <it_support@ebet.tw>
2021-07-16 18:21:07 +0800

    增加更新0716

--- js/header.js
+++ js/header.js
@@ -47,7 +47,7 @@ if (window.localStorage.getItem('lang') == "cn") {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="index.html">首页</a>
+                    <a class="nav-link" href="index.html">首页111</a>
                 </li>
                 <li class="nav-item">
                         <a class="nav-link" href="precautions.html">注意事项</a>
