1a17ab18e6fbb45eb830198eef04a2ffd0c4c8e5
IT_support <it_support@ebet.tw>
2021-10-20 18:34:12 +0800

    fortune roulette update 1020

--- js/tableSubType.js
+++ js/tableSubType.js
@@ -16,11 +16,11 @@ $(function () {
 const tableSubType = [
     { "code": "0","GameType": "Common", "TableType": "General Table" },
     { "code": "1","GameType": "Baccarat", "TableType": "VIP Table" },
-    { "code": "1","GameType": "Roulette", "TableType": "VIP Table(Fortune Roulette)" }
+    { "code": "1","GameType": "Roulette", "TableType": "Fortune Roulette" }
 ]
 
 const tableSubType_cn = [
     { "code": "0","GameType": "共同", "TableType": "一般桌" },
     { "code": "1","GameType": "百家乐", "TableType": "VIP桌" },
-    { "code": "1","GameType": "轮盘", "TableType": "VIP桌(财富轮盘)" }
+    { "code": "1","GameType": "轮盘", "TableType": "财富轮盘" }
 ]
