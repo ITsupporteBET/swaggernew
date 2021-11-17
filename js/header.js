95cab65b9e87de18a16f750b739d7eb38437a257
IT_support <it_support@ebet.tw>
2021-11-17 16:16:18 +0800

    new game minigame update 1117

--- js/header.js
+++ js/header.js
@@ -70,7 +70,7 @@ if (window.localStorage.getItem('lang') == "cn") {
 
                         <a class="dropdown-item" href="fortune_roulette.html" onclick="window.location.reload();">财富轮盘</a>
 
-                        <a class="dropdown-item" href="minigame.html" onclick="window.location.reload();">财富轮盘</a>
+                        <a class="dropdown-item" href="minigame.html" onclick="window.location.reload();">迷你游戏</a>
                     </div>
                 </li>
                 <li class="nav-item dropdown">
@@ -168,6 +168,8 @@ if (window.localStorage.getItem('lang') == "cn") {
                         <a class="dropdown-item" href="slot.html" onclick="window.location.reload();">Genesis slot</a>
 
                         <a class="dropdown-item" href="fortune_roulette.html" onclick="window.location.reload();">Fortune Roulette</a>
+
+                        <a class="dropdown-item" href="minigame.html" onclick="window.location.reload();">Mini-game</a>
                     </div>
                 </li>
                 <li class="nav-item dropdown">
